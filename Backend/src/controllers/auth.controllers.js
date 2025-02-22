2f55220d6bae260ef711103cb918254ca3c079e1
yatin Gaur <yatingaur66@gmail.com>
2025-02-22 23:17:20 +0530

    feat: add chat interface preview to SettingsPage; improve theme selection instructions

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -91,7 +91,7 @@ export const updateProfile = async (req, res) => {
   try {
     const { profilePic } = req.body;
     const userId = req.user._id;
-
+     console.log("update profile")
     if (!profilePic) {
       return res.status(400).json({ message: "Profile pic is required" });
     }
