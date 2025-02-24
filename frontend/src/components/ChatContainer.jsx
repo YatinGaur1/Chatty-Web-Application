14a5e4c10faa7f520765dc171e845706bcb5ac77
yatin Gaur <yatingaur66@gmail.com>
2025-02-24 08:31:49 +0530

    feat: implement message fetching in ChatContainer; add loading state and integrate online users in Sidebar

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore"
 
 
 export const ChatContainer = () => {
+  const {messages,getMessages,selectedUser,isMessagesLoading}=useChatStore();
+  useEffect(()=>{
+    getMessages(selectedUser._id)
+  },[selectedUser._id,getMessages])
+
+  if(isMessagesLoading) return <div>Loading...</div>
+  
   return (
     <div>ChatContainer</div>
   )
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,11 +2,12 @@ import { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
 import SidebarSkeleton from "../skeletons/SidebarSkeleton";
 import { Users } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
 export const Sidebar = () => {
   const { getUsers, users, setSelectedUser, selectedUser, isUsersLoading } =
     useChatStore();
 
-  const onlineUsers = [];
+  const {onlineUsers} = useAuthStore();
 
   useEffect(() => {
     getUsers();
