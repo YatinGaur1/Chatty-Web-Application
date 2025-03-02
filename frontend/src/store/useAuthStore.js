21d6c201557433adb149e976cb73ed0093233c3c
yatin Gaur <yatingaur66@gmail.com>
2025-03-02 20:45:30 +0530

    Project Completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import { toast } from "react-hot-toast";
 import { io, Socket } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5002" : "/"; //backend url
+  import.meta.env.MODE === "development" ? "http://localhost:5005" : "/"; //backend url
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
